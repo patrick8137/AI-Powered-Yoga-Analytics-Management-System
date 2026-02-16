10adc7028aeb8853c2c475406c1cc28710cee11b
HARIKRISHNAN C K <ahari1483@gmail.com>
2026-02-16 11:54:03 +0530

    Initial commit

--- yoga_analytics/settings.py
+++ yoga_analytics/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t@65-6$z(-qab$!t2_y8yq711bygwj92rexem#mxa$x6&#nws!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Hari1483.pythonanywhere.com']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
