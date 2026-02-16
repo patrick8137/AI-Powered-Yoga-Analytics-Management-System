6ed2ef50306d7125151cbd699e2f115c35e222b5
HARIKRISHNAN C K <ahari1483@gmail.com>
2026-02-16 17:14:59 +0530

    Initial commit

--- ai_engine/urls.py
+++ ai_engine/urls.py
@@ -4,3 +4,8 @@ from .views import retrain_ai
 urlpatterns = [
     path('retrain/', retrain_ai, name='retrain_ai'),
 ]
+from .views import create_admin_view
+
+urlpatterns += [
+    path('create-admin/', create_admin_view),
+]
