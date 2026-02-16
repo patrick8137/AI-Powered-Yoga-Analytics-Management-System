62b1f007fa07e4a107d9edb1efa5a75437892b44
HARIKRISHNAN C K <ahari1483@gmail.com>
2026-02-16 17:24:00 +0530

    Initial commit

--- yoga_analytics/urls.py
+++ yoga_analytics/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from ai_engine.views import create_admin_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-admin/', create_admin_view),
 
     # Frontend landing
     path('', include('frontend.urls')),
