9ca741c0264af138026ddfa82965374836097897
HARIKRISHNAN C K <ahari1483@gmail.com>
2026-02-17 21:10:53 +0530

    Initial commit

--- yoga_analytics/settings.py
+++ yoga_analytics/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = 'yoga_analytics.wsgi.application'
 # DATABASE
 # ========================
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # ========================
 # PASSWORD VALIDATION
