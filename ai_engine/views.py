a9273e693dc9fdf218d07e1f5b94ec225194765f
HARIKRISHNAN C K <ahari1483@gmail.com>
2026-02-18 10:48:38 +0530

    Initial commit

--- ai_engine/views.py
+++ ai_engine/views.py
@@ -7,16 +7,3 @@ from .services import run_ai_analysis
 def retrain_ai(request):
     run_ai_analysis()
     return HttpResponse("AI retraining completed successfully.")
-
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
-def create_admin_view(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            username='admin',
-            email='admin@example.com',
-            password='admin123'
-        )
-        return HttpResponse("Admin created")
-    return HttpResponse("Admin already exists")
